56188f984c04e99a7c633538e1dc1e2b7e33963b
Margaret Wolff <mwolff2@hb-5024.fhcrc.org>
2025-05-30 17:11:58 -0700

    Add beginning_analysis.R with initial analysis

--- beginning_analysis.R
+++ beginning_analysis.R
@@ -1,5 +1,10 @@
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("immunarch")           # Install the package
+
 library(immunarch)
 library(ggplot2)
+library(immunarch)
 
 # Load your data from the './Data/' directory
 imm_data <- repLoad("./Data/")
@@ -8,4 +13,11 @@ repExplore(imm_data$data, "lens") %>% vis()  # Visualise the length distribution
 repClonality(imm_data$data, "homeo") %>% vis()  # Visualise the relative abundance of clonotypes
 
 
+library(tidyverse)
 
+imm_data
+imm_data$meta
+
+repOverlap(imm_data$data) %>% vis()      # Compute and visualise the most important statistics:
+geneUsage(immdata$data[[1]]) %>% vis()  #     public clonotypes, gene usage, sample diversity
+repDiversity(immdata$data) %>% vis(.by = "Status", .meta = immdata$meta)      # Group samples
